9f5c25efeddaa3b846186353d547067dd976c843
aynazheydariafshar <aynazhadriafshar@gmail.com>
2025-04-23 16:14:21 +0330

    FEAT :add shadcn on project

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,12 +2,13 @@ import logo from "../../assets/images/logo.png";
 import { strings } from "./strings";
 import searchIcon from "../../assets/icons/search.svg";
 import basketIcon from "../../assets/icons/shopping-bag.svg";
+import { Button } from "../ui/elements/button/Button";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-5 px-11">
       <div>
-        <img className="w-50 h-xs" src={logo} alt="logo" />
+        <img className="w-5/12 h-xs" src={logo} alt="logo" />
       </div>
       <ul className="flex items-center gap-4 text-xl">
         <li>{strings.HOME}</li>
@@ -18,7 +19,7 @@ const Navbar = () => {
       <div className="flex items-center gap-8">
         <img src={searchIcon} alt="search" />
         <img src={basketIcon} alt="basket" />
-        <button>sign in</button>
+        <Button>sign in</Button>
       </div>
     </nav>
   );

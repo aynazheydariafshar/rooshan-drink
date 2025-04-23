a258f7834dc8fb6f3892ef11e3dcaab1a4ec1cef
aynazheydariafshar <aynazhadriafshar@gmail.com>
2025-04-23 17:12:27 +0330

    FIX :remove navigation and add li for menu

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -3,12 +3,6 @@ import { strings } from "./strings";
 import searchIcon from "../../assets/icons/search.svg";
 import basketIcon from "../../assets/icons/shopping-bag.svg";
 import { Button } from "../ui/elements/Button";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "../ui/fragments/navigation-menu";
 import classNames from "classnames";
 import { useState } from "react";
 
@@ -19,38 +13,35 @@ const Navbar = () => {
       <div>
         <img className="w-5/12 h-xs" src={logo} alt="logo" />
       </div>
-      <NavigationMenu>
-        <NavigationMenuList className="gap-5 font-bold">
-          <NavigationMenuItem
-            onClick={() => setMenu("home")}
-            className={classNames(
-              menu === "home" &&
-                "border-b-4 border-orange-500 transition-all duration-200"
-            )}
-          >
-            <NavigationMenuLink> {strings.HOME}</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem
-            onClick={() => setMenu("menu")}
-            className={classNames(
-              menu === "menu" &&
-                "border-b-4 border-orange-500 transition-all duration-200"
-            )}
-          >
-            <NavigationMenuLink> {strings.MENU} </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem
-            onClick={() => setMenu("contact-us")}
-            className={classNames(
-              menu === "contact-us" &&
-                "border-b-4 border-orange-500 transition-all duration-200"
-            )}
-          >
-            <NavigationMenuLink> {strings.CONTACT_US} </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-
+      <ul className="flex items-center gap-7 font-bold text-lg">
+        <li
+          onClick={() => setMenu("home")}
+          className={classNames(
+            menu === "home" &&
+              "border-b-4 border-orange-500 transition-all duration-200"
+          )}
+        >
+          {strings.HOME}
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={classNames(
+            menu === "menu" &&
+              "border-b-4 border-orange-500 transition-all duration-200"
+          )}
+        >
+          {strings.MENU}{" "}
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={classNames(
+            menu === "contact-us" &&
+              "border-b-4 border-orange-500 transition-all duration-200"
+          )}
+        >
+          {strings.CONTACT_US}{" "}
+        </li>
+      </ul>
       <div className="flex items-center gap-8">
         <img src={searchIcon} alt="search" />
         <img src={basketIcon} alt="basket" />
